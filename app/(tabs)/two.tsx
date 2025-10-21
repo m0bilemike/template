fe0758dcef236aa58500b65a3386a9df10bf1348
mobilemike <56070551+m0bilemike@users.noreply.github.com>
2025-10-21 23:11:34 +0100

    feat: basic layout

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,15 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { Header } from '@/components/ui/Header';
 
 export default function TabTwoScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+    <View style={{flex: 1}}>
+      <Header />
+      <View style={styles.container}>
+        <Text style={styles.title}>Tab Two</Text>
+      </View>
     </View>
   );
 }
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { Platform, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
+import SettingsCard from '@/components/settings/SettingsCard';
 import { Text, View } from '@/components/Themed';
+import { RootState } from '@/store';
+import { useSelector } from 'react-redux';
 
 export default function ModalScreen() {
+  const appUsage = useSelector((state: RootState) => state.appUsage);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
-
+      <SettingsCard label="App Opens" value={appUsage.totalOpens} icon="📱" />
+      <SettingsCard label="Daily Streak" value={appUsage.streak} icon="🔥" />
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
